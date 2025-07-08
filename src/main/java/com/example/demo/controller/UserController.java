8f5619753676a40e9c335d62cf1552bc65872e63
SilasLeao <leaosilas@gmail.com>
2025-07-08 15:46:33 -0300

    feat: navegação entre abas 1/2

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -19,18 +19,6 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private AccountService accountService;
-
-    @Autowired
-    private TransactionService transactionService;
-
-    @Autowired
-    private ExtratoService extratoService;
-
-    @Autowired
-    private OrcamentoService orcamentoService;
-
-    @Autowired
     private UserService userService;
 
     @Autowired
@@ -38,39 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/dashboard")
-    public String showDashboard(Model model, User user) {
-        model.addAttribute("user", user);
-        return "user/dashboard";
-    }
 
-    @GetMapping("/user/contas")
-    public String listAccounts(Model model) {
-        List<Account> accounts = accountService.findAll();
-        model.addAttribute("accounts", accounts);
-        return "user/contas";
-    }
-
-    @GetMapping("/user/transacoes")
-    public String listTransactions(Model model) {
-        List<Transaction> transactions = transactionService.findAll();
-        model.addAttribute("transactions", transactions);
-        return "user/transacoes";
-    }
-
-    @GetMapping("/user/extrato")
-    public String showExtrato(Model model) {
-        List<Transaction> extrato = extratoService.getExtratoDoMes();
-        model.addAttribute("extrato", extrato);
-        return "user/extrato";
-    }
-
-    @GetMapping("/user/orcamento")
-    public String showOrcamentoAnual(Model model) {
-        var planilha = orcamentoService.getPlanilhaOrcamento();
-        model.addAttribute("planilha", planilha);
-        return "user/orcamento";
-    }
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
